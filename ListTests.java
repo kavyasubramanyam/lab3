9a38529dba29ff15dc608a44de146605636fa55c
Kavya Subu <kavyasubu@Kavyas-Air.lan>
2022-10-14 21:54:41 -0700

    lab3

--- ListTests.java
+++ ListTests.java
@@ -1,13 +1,19 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.*;
 
 public class ListTests {
   @Test 
-  public void testFilter() {
-    List<String> inputList = { 3 };
-    ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+  public void testMerge() {
+    List<String> inputList1 = new ArrayList<>(Arrays.asList( "a", "b", "c" ));
+    List<String> inputList2 = new ArrayList<>(Arrays.asList( "a", "b", "c" ));
+    System.out.println(inputList1);
+   var myArray = ListExamples.merge(inputList1, inputList2);
+   List<String> checkList = new ArrayList<>(Arrays.asList( "a","a","b","b","c","c"));
+    System.out.println(myArray);
+    assertEquals(checkList, myArray);
 	}
+
 
 
 }
